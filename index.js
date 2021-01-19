c5f84e0f7aca041a5d6581ecad40f6852bc78618
Rebecca Sorrell <rebeccassorrell@gmail.com>
2021-01-19 21:17:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ rock.style.top = `${top += 2}px`;
      * we should call endGame().
      */
 
-if (window.)  
+if (window.checkCollision())  
     /**
      * Otherwise, if the rock hasn't reached the bottom of
      * the GAME, we want to move it again.
