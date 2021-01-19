d105e659b33b72aced5e7350224dceaa10042ca7
Rebecca Sorrell <rebeccassorrell@gmail.com>
2021-01-19 21:15:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ rock.style.top = `${top += 2}px`;
      */
 
 
-if(checkCollision(rock))
+if(window.checkCollision(rock))
   window endGame();
     /**
      * Otherwise, if the rock hasn't reached the bottom of
