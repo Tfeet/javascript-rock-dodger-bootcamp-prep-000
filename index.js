eb6a263e7e634119346ef2ff02c4f3f4e195a5fc
Rebecca Sorrell <rebeccassorrell@gmail.com>
2021-01-19 21:16:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ rock.style.top = `${top += 2}px`;
      * we should call endGame().
      */
 
-  
+if   
     /**
      * Otherwise, if the rock hasn't reached the bottom of
      * the GAME, we want to move it again.
